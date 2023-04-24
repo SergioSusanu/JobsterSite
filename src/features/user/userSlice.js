b5e50eb4e9435105c6cbb0dc7b7c28333fd1fa27
SergioSusanu <Sergio.susanu.developer@gmail.com>
2023-04-25 00:58:39 +0300

    profile page and functionality is done

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -30,6 +30,26 @@ async (user, thunkApi) => {
     }
 })
 
+export const updateUser = createAsyncThunk(
+    'user/updateUser',
+    async (user, thunkApi) => {
+        try {
+            const resp = await customFetch.patch("/auth/updateUser", user, {
+              headers: {
+                authorization: `Bearer ${thunkApi.getState().user.user.token}`,
+              },
+            });
+            return resp.data
+        } catch (error) {
+            if (error.response.status === 401) {
+                thunkApi.dispatch(logoutUser())
+                return thunkApi.rejectWithValue('Unauthorized! Logging out...') 
+            }
+            return thunkApi.rejectWithValue(error.response.data.msg)
+        }
+    }
+    )
+
 const userSlice = createSlice({
     name: 'user',
     initialState,
@@ -44,38 +64,52 @@ const userSlice = createSlice({
         }
     },
      extraReducers:(builder)=>{
-        builder.addCase(registerUser.pending, (state) =>{
-            console.log('pending');
-            state.isLoading = true
-        })
-        .addCase(registerUser.fulfilled, (state, resp) =>{
-            console.log('fulfilled');
+        builder
+          .addCase(registerUser.pending, (state) => {
+            console.log("pending");
+            state.isLoading = true;
+          })
+          .addCase(registerUser.fulfilled, (state, resp) => {
+            console.log("fulfilled");
             state.isLoading = false;
-            if (typeof resp.payload != 'undefined'){
-              const {user} = resp.payload;
-              state.user = user
-              toast.success(`Hello there ${user.name}`)
-              addUserToLocalStorage(state.user)
-            } else toast.error('some error')     
-        })
-        .addCase(registerUser.rejected, (state, {payload})=>{
-            console.log('rejected');
+            if (typeof resp.payload != "undefined") {
+              const { user } = resp.payload;
+              state.user = user;
+              toast.success(`Hello there ${user.name}`);
+              addUserToLocalStorage(state.user);
+            } else toast.error("some error");
+          })
+          .addCase(registerUser.rejected, (state, { payload }) => {
+            console.log("rejected");
             state.isLoading = false;
-            toast.error(payload)
-        })
-        .addCase(loginUser.pending, (state) => {
-            state.isLoading = true
-        })
-        .addCase(loginUser.fulfilled, (state, {payload})=>{
-            state.isLoading = false
-            state.user = payload.user
-            toast.success('Welcome back ' + state.user.name)
+            toast.error(payload);
+          })
+          .addCase(loginUser.pending, (state) => {
+            state.isLoading = true;
+          })
+          .addCase(loginUser.fulfilled, (state, { payload }) => {
+            state.isLoading = false;
+            state.user = payload.user;
+            toast.success("Welcome back " + state.user.name);
             addUserToLocalStorage(state.user);
-        })
-        .addCase(loginUser.rejected, (state, {payload})=>{
-            state.isLoading = false
-            toast.error(payload)
-        })
+          })
+          .addCase(loginUser.rejected, (state, { payload }) => {
+            state.isLoading = false;
+            toast.error(payload);
+          })
+          .addCase(updateUser.pending, (state) => {
+            state.isLoading = true;
+          })
+          .addCase(updateUser.fulfilled, (state, { payload }) => {
+            state.isLoading = false;
+            state.user = payload.user;
+            toast.success("User updated " + state.user.name);
+            addUserToLocalStorage(state.user);
+          })
+          .addCase(updateUser.rejected, (state, { payload }) => {
+            state.isLoading = false;
+            toast.error(payload);
+          });
     }
 })
 
